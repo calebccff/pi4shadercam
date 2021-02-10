5170e2e7325551bb4640058f6519952b2813bb62
Caleb Connolly <caleb@connolly.tech>
2021-02-10 21:10:28 +0000

    WIP: raspberrypi4: initial support
    The pi4 uses a very different kernel config to the pi0, the hardware is
    also very different so using a base config doesn't seem like the
    sensible approach here.

--- build-showmewebcam.sh
+++ build-showmewebcam.sh
@@ -2,6 +2,7 @@
 
 # Allow to overwrite default buildroot location
 BUILDROOT_DIR=${BUILDROOT_DIR:-buildroot}
+KERNEL_CONFIG=${KERNEL_CONFIG:-raspberrypi0}
 
 if [ ! -d "$BUILDROOT_DIR" ]; then
   echo "Please provide a valid buildroot directory, either by using BUILDROOT_DIR or by renaming it to \"buildroot\""
@@ -15,9 +16,12 @@ case "$BOARDNAME" in
   ;;
   raspberrypi0w)
   ;;
+  raspberrypi4)
+    KERNEL_CONFIG="raspberrypi4"
+  ;;
   *)
     echo "usage: BUILDROOT_DIR=buildroot $0 (boardname)"
-    echo "boardname: raspberrypi0, raspberrypi0w"
+    echo "boardname: raspberrypi0, raspberrypi0w, raspberrypi4"
     exit 1
   ;;
 esac
@@ -28,6 +32,7 @@ sed "1i ### DO NOT EDIT, this file was automatically generated\n" -i "configs/${
 
 # Merge kernel configurations
 if [ -f "board/linux-${BOARDNAME}.config" ]; then
+  echo "MERGING KERNEL CONFIGS================="
   KCONFIG_CONFIG="board/linux.config" "$BUILDROOT_DIR/support/kconfig/merge_config.sh" -m -r board/linux-base.config "board/linux-${BOARDNAME}.config"
 else
   cp board/linux-base.config board/linux.config
