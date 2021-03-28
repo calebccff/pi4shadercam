a06421bcaf8895f2e458dabf6bc431829d2be774
Caleb Connolly <caleb.connolly@ideasonboard.com>
2021-03-28 20:21:00 +0100

    cleaning up packages / uvc-gadget debugging

--- package/uvc-gadget/start-webcam.sh
+++ package/uvc-gadget/start-webcam.sh
@@ -22,4 +22,4 @@ else
   logger -t "$LOGGER_TAG" "No camera.txt found in boot"
 fi
 
-#/opt/uvc-webcam/uvc-gadget -l -p 21 -b 3 -u /dev/video1 -v /dev/video0
+/opt/uvc-webcam/uvc-gadget -c /dev/video0 uvc.usb0
--- package/uvc-gadget/uvc-gadget.mk
+++ package/uvc-gadget/uvc-gadget.mk
@@ -4,28 +4,30 @@
 #
 ################################################################################
 
-UVCGADGET_VERSION = 105134f9a7a3faad9f02a6a5516a8cd8e625fb04
-UVCGADGET_SITE = git://github.com/calebccff/uvc-gadget
-UVCGADGET_LICENSE = GPL-2.0+
-UVCGADGET_LICENSE_FILES = LICENSE
-UVCGADGET_DEST_DIR = /opt/uvc-webcam
-UVCGADGET_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=$(BASE_DIR)/host/usr/share/buildroot/toolchainfile.cmake
-UVCGADGET_SITE_METHOD = git
-UVCGADGET_INIT_SYSTEMD_TARGET = basic.target.wants
+UVC_GADGET_VERSION = fa2ffedb1efc7b880b7beaf315b8686456fb8a04
+UVC_GADGET_SITE = git://github.com/calebccff/uvc-gadget
+UVC_GADGET_LICENSE = GPL-2.0+
+UVC_GADGET_LICENSE_FILES = LICENSE
+UVC_GADGET_DEST_DIR = /opt/uvc-webcam
+UVC_GADGET_LIB_DIR = /usr/lib
+UVC_GADGET_CONF_OPTS = -DCMAKE_TOOLCHAIN_FILE=$(BASE_DIR)/host/usr/share/buildroot/toolchainfile.cmake
+UVC_GADGET_SITE_METHOD = git
+UVC_GADGET_INIT_SYSTEMD_TARGET = basic.target.wants
 
-define UVCGADGET_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)$(UVCGADGET_DEST_DIR)
-	$(INSTALL) -D -m 0755 $(@D)/uvc-gadget $(TARGET_DIR)$(UVCGADGET_DEST_DIR)
-	$(INSTALL) -D -m 0755 $(UVCGADGET_PKGDIR)/multi-gadget.sh $(TARGET_DIR)$(UVCGADGET_DEST_DIR)
-	$(INSTALL) -D -m 0755 $(UVCGADGET_PKGDIR)/start-webcam.sh $(TARGET_DIR)$(UVCGADGET_DEST_DIR)
+define UVC_GADGET_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)$(UVC_GADGET_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/uvc-gadget $(TARGET_DIR)$(UVC_GADGET_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(@D)/lib/libuvcgadget.so $(TARGET_DIR)$(UVC_GADGET_LIB_DIR)
+	$(INSTALL) -D -m 0755 $(UVC_GADGET_PKGDIR)/multi-gadget.sh $(TARGET_DIR)$(UVC_GADGET_DEST_DIR)
+	$(INSTALL) -D -m 0755 $(UVC_GADGET_PKGDIR)/start-webcam.sh $(TARGET_DIR)$(UVC_GADGET_DEST_DIR)
 endef
 
-define UVCGADGET_INSTALL_INIT_SYSTEMD
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/$(UVCGADGET_INIT_SYSTEMD_TARGET)
-	$(INSTALL) -D -m 644 $(UVCGADGET_PKGDIR)/uvc-webcam.service $(TARGET_DIR)/usr/lib/systemd/system/uvc-webcam.service
-	$(INSTALL) -D -m 644 $(UVCGADGET_PKGDIR)/usb-gadget-config.service $(TARGET_DIR)/usr/lib/systemd/system/usb-gadget-config.service
-	ln -sf /usr/lib/systemd/system/uvc-webcam.service $(TARGET_DIR)/etc/systemd/system/$(UVCGADGET_INIT_SYSTEMD_TARGET)
-	ln -sf /usr/lib/systemd/system/usb-gadget-config.service $(TARGET_DIR)/etc/systemd/system/$(UVCGADGET_INIT_SYSTEMD_TARGET)
+define UVC_GADGET_INSTALL_INIT_SYSTEMD
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/$(UVC_GADGET_INIT_SYSTEMD_TARGET)
+	$(INSTALL) -D -m 644 $(UVC_GADGET_PKGDIR)/uvc-webcam.service $(TARGET_DIR)/usr/lib/systemd/system/uvc-webcam.service
+	$(INSTALL) -D -m 644 $(UVC_GADGET_PKGDIR)/usb-gadget-config.service $(TARGET_DIR)/usr/lib/systemd/system/usb-gadget-config.service
+	ln -sf /usr/lib/systemd/system/uvc-webcam.service $(TARGET_DIR)/etc/systemd/system/$(UVC_GADGET_INIT_SYSTEMD_TARGET)
+	ln -sf /usr/lib/systemd/system/usb-gadget-config.service $(TARGET_DIR)/etc/systemd/system/$(UVC_GADGET_INIT_SYSTEMD_TARGET)
 endef
 
 $(eval $(cmake-package))
